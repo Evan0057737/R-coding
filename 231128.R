3df1302ca6a22f350bed2bb8c7824a56cdde9933
Evan0057737 <144879223+Evan0057737@users.noreply.github.com>
2023-12-06 15:04:05 +0800

    231206

--- 231128.R
+++ 231128.R
@@ -3,8 +3,9 @@ jobcode<- jsonlite::fromJSON("data/jobcode.json")
 jobcool= "data/job.json"
 jobcode2="data/jobcode.json"
 insurance <- jsonlite::fromJSON("data/insurance.json")
-
-
+die <- jsonlite::fromJSON("data/225684.json")
+air <- jsonlite::fromJSON("data/air.json")
+money <- jsonlite::fromJSON("data/company.json")
 
 library(jsonlite)
 library(dplyr)
@@ -26,5 +27,45 @@ result <- df %>%
 
 # 打印排序後的結果
 print(result)
+
+# 讀取JSON檔案
+json_data2 <- fromJSON("C:/R-coding/data/225684.json")
+
+# 將JSON數據轉換為data.frame
+df5 <- as.data.frame(json_data2)
+
+# 假設 "平均投保薪資" 欄位中包含非數值數據
+df5$`人數-合計` <- as.numeric(df5$`人數-合計`)
+
+# 使用dplyr套件進行分析和排序
+result <- df5 %>%
+  group_by(死亡原因) %>%              # 假設"業別"是JSON中的相應欄位
+  summarize(`人數-合計` = mean(`人數-合計`)) %>%
+  arrange(desc(`人數-合計`))     # 按照平均投保薪資由高到低排序
+
+# 打印排序後的結果
+print(result)
+
+
+json_data4 <- fromJSON("C:/R-coding/data/company.json")
+
+# 將JSON數據轉換為data.frame
+df6 <- as.data.frame(json_data4)
+
+# 假設 "平均投保薪資" 欄位中包含非數值數據
+df6$`營業收入-去年同月增減(%)` <- as.numeric(df6$`營業收入-去年同月增減(%)`)
+
+# 使用dplyr套件進行分析和排序
+result <- df6 %>%
+  group_by(產業別) %>%              # 假設"業別"是JSON中的相應欄位
+  summarize(`營業收入-去年同月增減(%)` = mean(`營業收入-去年同月增減(%)`)) %>%
+  arrange(desc(`營業收入-去年同月增減(%)`))     # 按照平均投保薪資由高到低排序
+
+# 打印排序後的結果
+print(result)
+
+
+
+json_data5 <- fromJSON("C:/R-coding/data/metro.json")
 
 
---
+++ 231206.R
@@ -0,0 +1,18 @@
+values <- c(1,2,3,4,5)
+
+seq_along(values)
+for(.x in values)
+  myfun <- function(values)
+x=17
+myfun(x)
+
+oneComingStudentMajor <- sample(c("Economics", "Sociology"), 1)
+
+
+# oneComingStudentMajor == "Economics"
+if(oneComingStudentMajor == "Economics")
+{print("Please go to room 201.")}
+
+# oneComingStudentMajor == "Sociology"
+if(oneComingStudentMajor == "Socialogy")
+{print("Please go to room 301.")}
