903a6a4dbcc159fe184b3fb13b88fef6b3200775
Evan0057737 <144879223+Evan0057737@users.noreply.github.com>
2023-12-20 14:08:08 +0800

    Update 期末v7~v9.R

--- 期末v7~v9.R
+++ 期末v7~v9.R
@@ -1,40 +1,5 @@
-install.packages("readxl")
 data5 <- readxl::read_excel("data/metro.xlsx")
-
-number <- data5$V7 |> as.numeric()
-sortnumber <- sort(number, decreasing=TRUE,index.return = TRUE)
-print(sortnumber)
-
-number2 <- data5$V8 |> as.numeric()
-sortnumber2 <- sort(number2, decreasing=TRUE,index.return = TRUE)
-print(sortnumber2)
-
-number3 <- data5$V9 |> as.numeric()
-sortnumber3 <- sort(number3, decreasing=TRUE,index.return = TRUE)
-print(sortnumber3)
-
-number4 <- data5$V1 |> as.numeric()
-sortnumber4 <- sort(number4, decreasing=TRUE,index.return = TRUE)
-print(sortnumber4)
-
-number5 <- data5$V2 |> as.numeric()
-sortnumber5 <- sort(number5, decreasing=TRUE,index.return = TRUE)
-print(sortnumber5)
-
-number6 <- data5$V3 |> as.numeric()
-sortnumber6 <- sort(number6, decreasing=TRUE,index.return = TRUE)
-print(sortnumber6)
-
-data5$...1[c(46,76,45,82,79,72,73,65,80,40)]#107進
-data5$...1[c(46,76,45,82,72,79,73,65,80,40)]#107出
-data5$...1[c(46,76,45,82,79,72,73,65,40,89)]#108進
-
-data5$...1[c(46,45,76,82,79,65,73,80,72,51)]#104進
-data5$...1[c(46,76,45,82,79,80,72,65,73,51)]#104出
-data5$...1[c(46,45,76,82,79,65,73,72,80,51)]#105進
-
-
-install.packages(c("ggplot2", "gcookbook")) 
+#總進站折線圖
 library(ggplot2) 
 library(gcookbook) 
 
@@ -58,7 +23,7 @@ ggplot(taipeimetro, aes(x = year, y = value)) +
   labs(x = "年份", y = "人數(百萬)", title = "總進站折線圖") +
   scale_x_continuous(breaks = c(104,105,106,107, 108, 109,110,111), labels = c("104","105","106","107","108","109","110","111")
 )
-
+#104~108年人數改變量(斜率)
 values <- c(716.575283,
           788.970453
 )
@@ -75,6 +40,7 @@ change_rate <- function(values) {
 result <- change_rate(values)
 print(result)
 
+#108~110人數改變量(斜率)
 values2 <- c( 788.970453,
               530.912219)
 change_rate <- function(values) {
@@ -89,6 +55,21 @@ change_rate <- function(values) {
 result2 <- change_rate(values2)
 print(result2)
 
+#110~111人數改變量(斜率)
+values3 <- c(530.912219,587.017562)
+change_rate <- function(values) {
+  new_values <- values[-1]
+  old_values <- values[-length(values)]
+  
+  rate <- (new_values - old_values) / old_values * 100
+  
+  return(rate)
+}
+
+result3 <- change_rate(values3)
+print(result3)
+
+#進出站相關係數
 co108 <- data.frame(
   X <- c( 788970453 , 32213290 , 28021574 , 26383018  ,22918425 , 15508892  ,15431724 , 14942869 , 14472026,
             13048812 , 12774960 , 12768222 , 12557498  ,12529506,  11839165 , 11703409,  11599492 , 10960768,
@@ -123,16 +104,158 @@ cor108 <- cor(co108$X....c.788970453..32213290..28021574..26383018..22918425..15
 print(paste("相關係數",cor108))
 
 
-data5$...1 |> 
-  factor() ->　data5$mrtstop
-levels(data5$mrtstop)[c(1,4, 3, 2)] <- c("a","a","b", "b")
-data5$isbannan <- logical(120)
-data5$isbannan[c(66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87)]= TRUE
-data5$isxinyi <- logical(120)
-data5$isxinyi[c(44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,88,89,90,91,92,113)]= TRUE
-data5$iszhonghe <- logical(120)
-data5$iszhonghe[c(100,101,102,103,104,105,106,107,108,109,110,111,112,114,115,116,117,118,119,120)]= TRUE
-data5$issonshan<- logical(120)
-data5$issonshan[c(28,29,30,31,32,33,34,35,36,37,38,39,93,94,95,96,97,98,99)]=TRUE
-data5$iswenhu <- logical(120)
-data5$iswenhu[c(4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30)]= TRUE
+# 複製轉運站-----
+# 複製第38列、做成中正紀念堂R
+new_row1 <- data5[38,]
+names(new_row1) <- names(data5)
+data5 <- rbind(data5, new_row1)
+
+
+# 複製第37列、做成古亭Y
+new_row2 <- data5[37,]
+names(new_row2) <- names(data5)
+data5 <- rbind(data5, new_row2)
+
+# 複製第76列、做成西門G
+new_row3 <- data5[76,]
+names(new_row3) <- names(data5)
+data5 <- rbind(data5, new_row3)
+
+
+# 複製第113列、做成東門R
+new_row4 <- data5[113,]
+names(new_row4) <- names(data5)
+data5 <- rbind(data5, new_row4)
+
+# 直接索引刪除指定行
+na.omit(data5, inplace = TRUE)
+data5 <- na.omit(data5)
+
+#調整列數
+rownames(data5) <- NULL
+
+
+
+# 指定分組的索引108年進站
+numberV9 <- data5$V9 |> as.numeric()
+v9_data5 <- data5[ 4:124, 10, drop = FALSE]
+v9_data5 <- as.data.frame((v9_data5))
+View(v9_data5)
+numberV9data5 <- as.numeric(v9_data5$V9)
+rownames(v9_data5) <- NULL
+
+
+
+# 板南線------
+group_indices1 <- c(42, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84)
+selected_data1 <- v9_data5[group_indices1, ]
+selected_data1 <- as.numeric(selected_data1)
+
+# 找到最大值和最小值對應的行索引
+max_value1 <- max(selected_data1)
+min_value1 <- min(selected_data1)
+mean_value1 <- mean(selected_data1)
+row_index_max1 <- which(selected_data1 == max_value1)
+row_index_min1 <- which(selected_data1 == min_value1)
+#data[c(73+3,65+3),c(1)] #這裡回傳的是list的順序，所以算上面的數字再把列數加三
+row_index_max1
+row_index_min1 
+
+data5[c(76,68),c(1)]
+
+cat("板南線108年進站資訊")
+cat("最大值:", data5[c(76),c(1)], max_value1)
+cat("最小值:", data5[c(68),c(1)], min_value1)
+cat("平均值:", mean_value1, "\n")
+
+
+
+# 淡水信義線-----
+group_indices2 <- c(41,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,85,86,87,88,89,18,121)
+selected_data2 <- v9_data5[group_indices2, ]
+selected_data2 <- as.numeric(selected_data2)
+
+# 找到最大值和最小值對應的行索引
+max_value2 <- max(selected_data2)
+min_value2 <- min(selected_data2)
+mean_value2 <- mean(selected_data2)
+row_index_max2 <- which(selected_data2 == max_value2)
+row_index_min2 <- which(selected_data2 == min_value2)
+row_index_max2
+row_index_min2
+#data[c(a+3,b+3),c(1)]
+data5[c(46,61),c(1)]
+
+cat("淡水信義線108年進站資訊")
+cat("最大值:", data5[c(46),c(1)], max_value2)
+cat("最小值:", data5[c(61),c(1)], min_value2)
+cat("平均值:", mean_value2, "\n")
+
+
+
+
+# 中和新蘆線------------
+group_indices3 <- c(37,38,39,40,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,119)
+selected_data3 <- v9_data5[group_indices3, ]
+selected_data3 <- as.numeric(selected_data3)
+
+# 找到最大值和最小值對應的行索引
+max_value3 <- max(selected_data3)
+min_value3 <- min(selected_data3)
+mean_value3 <- mean(selected_data3)
+row_index_max3 <- which(selected_data3 == max_value3)
+row_index_min3 <- which(selected_data3 == min_value3)
+row_index_max3
+row_index_min3
+#data[c(a+3,b+3),c(1)]
+data5[c(40,103),c(1)]
+
+cat("中和新蘆線108年進站資訊")
+cat("最大值:", data5[c(40),c(1)], max_value3)
+cat("最小值:", data5[c(103),c(1)], min_value3)
+cat("平均值:", mean_value3, "\n")
+
+
+
+#松山新店線-----
+group_indices4 <- c(25,26,27,28,29,30,31,32,33,34,35,36,90,91,92,93,94,95,96,120)
+selected_data4 <- v9_data5[group_indices4, ]
+selected_data4 <- as.numeric(selected_data4)
+
+# 找到最大值和最小值對應的行索引
+max_value4 <- max(selected_data4)
+min_value4 <- min(selected_data4)
+mean_value4 <- mean(selected_data4)
+row_index_max4 <- which(selected_data4 == max_value4)
+row_index_min4 <- which(selected_data4 == min_value4)
+row_index_max4
+row_index_min4
+#data[c(a+3,b+3),c(1)]
+data5[c(123,28),c(1)]
+
+cat("松山新店線108年進站資訊")
+cat("最大值:", data5[c(123),c(1)], max_value4)
+cat("最小值:", data5[c(28),c(1)], min_value4)
+cat("平均值:", mean_value4, "\n")
+
+
+#文湖線-----
+group_indices5 <- c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)
+selected_data5 <- v9_data5[group_indices5, ]
+selected_data5 <- as.numeric(selected_data5)
+
+# 找到最大值和最小值對應的行索引
+max_value5 <- max(selected_data5)
+min_value5 <- min(selected_data5)
+mean_value5 <- mean(selected_data5)
+row_index_max5 <- which(selected_data5 == max_value5)
+row_index_min5 <- which(selected_data5 == min_value5)
+row_index_max5
+row_index_min5
+#data[c(a+3,b+3),c(1)]
+data5[c(27,14),c(1)]
+
+cat("文湖線線108年進站資訊")
+cat("最大值:", data5[c(27),c(1)], max_value5)
+cat("最小值:", data5[c(14),c(1)], min_value5)
+cat("平均值:", mean_value5, "\n")
